e0555e8435ce67837cdd7afac1e6f8dd3e607259
aboikliev <atanas.boikliev@gmail.com>
2014-09-30 01:45:35 +0300

    Solution to task 2 of HackBulgaria's Android tasks.

---
+++ Glob.java
@@ -0,0 +1,84 @@
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Glob {
+	private static final Logger log = Logger.getLogger(Glob.class.getName());
+	private String globString;
+	private Pattern pattern;
+
+	static {
+		ConsoleHandler h = new ConsoleHandler();
+		h.setLevel(Level.ALL);
+		h.setFormatter(new SimpleFormatter());
+	}
+
+	private Glob(String glob, Pattern pattern) {
+		globString = glob;
+		this.pattern = pattern;
+	}
+
+	private static String createRegexFromGlob(String glob) {
+		String result = "";
+		int brackets = 0;
+		for (int i = 0; i < glob.length(); ++i) {
+			final char character = glob.charAt(i);
+			switch (character) {
+			case '*':
+				if (glob.charAt(i + 1) == '*') {
+					result += ".*";
+					++i;
+					break;
+				}
+				else {
+					result += "\\w*";
+					break;
+				}
+			case '?':
+				result += '.';
+				break;
+			case '{': {
+				brackets++;
+				result += '(';
+				break;
+			}
+			case '}': {
+				--brackets;
+				result += '(';
+				break;
+			}
+			case ',':
+				if (brackets > 0) {
+					result += '|';
+				}
+				else {
+					result += ',';
+				}
+				break;
+			default:
+				result += character;
+			}
+		}
+		log.log(Level.INFO, "globe RegEx:" + result);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return globString;
+	}
+
+	public boolean matches(String path) {
+		Matcher m = pattern.matcher(path);
+		return m.matches();
+	}
+
+	public static Glob compile(String glob) {
+		log.log(Level.INFO, "Creating Glob from string:" + glob);
+		return new Glob(glob, Pattern.compile(createRegexFromGlob(glob)));
+	}
+
+}
---
+++ GlobTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+//use with -Djava.util.logging.SimpleFormatter.format="%4$s: %5$s %n"
+public class GlobTest {
+
+	private static Logger log = Logger.getLogger(GlobTest.class.getName());
+
+	@Test
+	public void testQuestionMark() {
+		log.log(Level.INFO, "testQuestionMark");
+		Glob glob = Glob.compile("abc?");
+		assertFalse(glob.matches("abc")); // false => ? means exactly one character
+		assertTrue(glob.matches("abcd")); // true
+		assertFalse(glob.matches("abcde"));
+	}
+
+	@Test
+	public void testStar() {
+		log.log(Level.INFO, "testStar");
+		Glob glob2 = Glob.compile("a*d");
+		assertTrue(glob2.matches("abcd"));
+		assertTrue(glob2.matches("abcujawiohtguahwuthawitthawuithawuthaithawtawutd"));
+		assertFalse(glob2.matches("abcdej"));
+		assertFalse(glob2.matches("abcujawiohtguahwuthawitthawuithawuthaithawtawutd1"));
+	}
+
+	@Test
+	public void testStarWithFolders() {
+		log.log(Level.INFO, "testStarWithFolders");
+		Glob glob3 = Glob.compile("*.html");
+		assertTrue(glob3.matches("index.html"));
+		assertFalse(glob3.matches("index.htm")); // false - missing 'l'
+		assertFalse(glob3.matches("directory/index.html")); // false - crossing directory boundaries
+	}
+
+	@Test
+	public void testDoubleStar() {
+		log.log(Level.INFO, "testDoubleStar");
+		Glob glob5 = Glob.compile("/home/georgi/**index.html");
+		assertTrue(glob5.matches("/home/georgi/testme/testme2/index.html"));
+		assertTrue(glob5.matches("/home/georgi/testme/testme2/testME_index.html"));
+		assertFalse(glob5.matches("/home/index.html"));
+
+		Glob glob = Glob.compile("**index.html");
+		assertTrue(glob.matches("/home/index.html"));
+	}
+
+	@Test
+	public void testComposite() {
+		log.log(Level.INFO, "testComposite");
+		Glob glob5 = Glob.compile("/home**/test?/*list*/index.html");
+		assertTrue(glob5.matches("/home/georgi/testme/test2/list/index.html"));
+		assertTrue(glob5.matches("/home/georgi/testme/test2/list1/index.html"));
+		assertTrue(glob5.matches("/home/georgi/testme/test2/1list/index.html"));
+		assertTrue(glob5.matches("/home/georgi/testme/test2/1list1/index.html"));
+		assertFalse(glob5.matches("/home/georgi/testme/test/list/index.html"));
+
+		assertTrue(glob5.matches("/home/test2/list/index.html"));
+		assertTrue(glob5.matches("/home/georgi/testme/test1/list/index.html"));
+	}
+}
